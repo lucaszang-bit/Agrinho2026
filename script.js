930234a80382f740971c5650a5c65abcbe5fec39
lucaszang-bit <lucas.zang@escola.pr.gov.br>
2026-06-03 09:30:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Menu Mobile
-const mobileMenu = document.getElementById('mobileMenu');
-const navLinks = document.getElementById('navLinks');
 
-mobileMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    
-    const icon = mobileMenu.querySelector('i');
-    if (navLinks.classList.contains('active')) {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-xmark');
-    } else {
-        icon.classList.remove('fa-xmark
